b896b90a5a7aeb420cceb873725fc26ed169a977
André Guil <126714001+Guil61@users.noreply.github.com>
2024-08-21 19:22:03 -0300

    Update textos1.py

--- apc/textos1.py
+++ apc/textos1.py
@@ -1,56 +1,148 @@
-texto_port = """Nosso povo Aqui começa a história do tempo dos antigos O início da história
- /01 Antes do mundo existir Ngutapa já existia. Ele não teve pai nem mãe. Mapana, a mulher de Ngutapa, se criou junto com ele. 
- /02 No mesmo lugar viviam também Baia e sua mulher. Baia era parente de Ngutapa. No lugar onde esses quatro se criaram é onde ficava a montanha Taiwegüne. E no igarapé Tonetü.
- /03 Naquele tempo, a terra ainda estava se formando. O mato era baixinho e o rio ainda tinha pouca água. Lá eles viviam. Passaram-se muitos anos. 
- /04 Ngutapa e Mapana nunca habitaram juntos. Nunca tiveram filhos. Um dia, quando o mato já estava crescido, Ngutapa foi caçar com Mapana.
- /05 No caminho eles começaram a brigar. Ngutapa agarrou sua mulher e lhe deu uma surra. 
- /06 Depois disso, amarrou Mapana num pau, de braços e pernas abertos. Deixou ela ali e seguiu para caçar no mato. Vieram as cabas e as formigas e morderam a sua periquita. 
- /07 Ela sofreu muito. 
- /08 Então apareceu o pássaro canea e se sentou no alto do pau onde ela estava amarrada. Mapana disse para o canea:
- /09 - Vovó, pode me desamarrar? O pássaro gritou:
- /10 - Co, co, co, cou! 
- /11 - Vovó, venha me desamarrar. Aquele desgraçado me prendeu aqui para me matar - ela falou de novo. 
- /12 O canea se transformou em gente e, chegando mais perto, perguntou: 
- /13 - O que lhe aconteceu, minha neta? Se você quer se vingar de 
- /14 Ngutapa, está aqui a caba. 
- /15 Ela pegou o caba e guardou. 
- /16 A casa de caba era muito grande, mas parecia pequena. O Canea falou ainda: 
- /17 - Você não pode ficar aí. Tem que esperar o seu marido num lugar onde ele não possa ver. 
- /18 Depois disso, o canea se transformou em pássaro e foi embora. 
- /19 Demorou um pouco e Ngutapa voltou da caçada. Vinha tocando flauta e pulava numa perna e noutra, cantando: 
- /20 - Por onde anda Mapana?! As cabas e as formigas morderam a periquita dela! Tcheruru tcheruru-u-u-u ... Tcheruru tcheruru-u-u-u ... - assim dizia. 
- /21 Mapana estava escondida num tronco de árvore, esperando Ngutapa passar. Escutou esse canto e se preparou. 
- /22 Quando ele chegou, ela jogou a casa de caba em cima dele e acertou-lhe os dois joelhos. Ngutapa caiu e não se levantou mais. 
- /23 Mapana deixou ele ali mesmo e foi embora. Ngutapa foi se arrastando até em casa. Desde que as cabas ferraram, seus joelhos começaram a inchar.""" 
+texto_port = """
+NOSSO POVO
+Aqui começa a história do tempo dos antigos 
+O início da história 
+/// 01 Nüma ga Ngutapa ga naãne nama'ã ya ĩĩtchicü,
+/// 02 rü nge'eǖ rü na tauma i ta'acü i nhũma na yema i na i aẽǖrü'ǖ.
+///03 Rü yiĩ'ǖ ngema na yamareǖ ga Ngutapa.
+/// 04 I nori'ǖtchicü,
+/// 05 ingeguma,
+/// 06 rü norü tchiura ta ngemana yaǖ,
+/// 07 rü ya Baia rü ta cünanangemaǖ rü norü tchiura rü ta nangemaǖ.
+/// 08 Erü nüma ga Baia rü Ngutapa tanü'ǖ ni'ĩ rü wü'iva na yae.
+/// 09 Nucüma ngeguma rü numa ga Ngutapa rü na tauǖma ga norü bue nu'ǖ tayema yerü tauguma nügüga nidau,
+/// 10 ga namamaã rü na yaemare.
+/// 11 Ngeguma yema namamare i ya'ĩ,
+/// 12 rü tauguma i buǖã.
+/// 13 Ga ngĩma ga Mapana,
+/// 14 rü yemaca ngitchi naai ga nüma ga Ngutapa.
+/// 15 Inacü i ngeguma i ngema.
+/// 16 I Taiwegüne i nori naãene i üǖwacü rü ngema ni'ĩ i nangemagüǖ erü ngema i nori naü'ǖ i naãene nua natü i Tonetü.
+/// 17 Erü yeguma ãrü naãenecü rü wü'i i ngewaca i ya eaneǖrǖ'ǖ ni'ĩ yerü natauma ga nhãtü rü yema nawa nayemagüǖ ricatama ni'ĩ.
+/// 18 Rü yeguma ngemamã ni'ĩ i nangemagü'ǖ,
+/// 19 rü marü nhurema ya taunecǖ na yemagü rü yegumama ni'ĩ ga tüǖ nacuaiǖ ga namã ga Ngutapa rü yeguma rü na tauma ga to,
+/// 20 ga duǖ'ǖ nüĩcagümare ni'ĩ ga nayemagü'ǖ.
+/// 21 Natürü ga guma Baia rü nü'ǖ tayema ga bue rü naaãcü nǖetcha nüma ga guma.
+/// 22 Ngutapa rü tauguma naãcü rü mu'utchicüma ya tauenecü na ngeacü.
+/// 23 Rü yeguma fenewa tümamãã na u'ǖ ga namá ga Ngutapa,
+/// 24 yerü yeguma rü marü nayaanegü ga naĩnecü rü yema i na feneeügu ni'ĩ ga tü'ǖ na cuai'ǖ ga nama,
+/// 25 rü yeguma tü'ǖ ya naĩ'ü rü nainewa.
+/// 26 tü'ǖ ni'ĩ nai rü ngiparawa rüta tü'ǖ ni'ĩ naĩgü rü ngĩtchacü'ǖwa rüta.
+/// 27 Rü yemawena rü nüma ga.
+/// 28 Ngutapa rü na üpetü ga na ya feneǖ.
+/// 29 Yerü yegumarü marü auriǖtchima nguǖ tinge rü maẽ rü tü'ǖ nawi rü ẽne rü tü'ü na ngo ga tümaãrü ngaǖwa.
+/// 30 Rü Yeguma tümaetügu i na rü wagügü i coou rü ngeguma ngina tacagü rü nhãtagürü:
+/// 31 - Pa noẽ,
+/// 32 pa coou tautchiname'ǖ ega tchoǖ icuwẽgǖgu nhãtagürü co-co-co-cou nhatagüǖ?
+/// 33 - tauchina meǖ ega tchoǖ icuiwẽgügu rü tchoǖ ni'ĩ ma i ucaǖǖtchi i Ngutapa nhãtagürü:
+/// 34 - Rü yeguma ga noẽ ga Co, co, co, cou rü ĩrü duǖ!
+/// 35 - pa noẽ,
+/// 36 Rü yeguma rü tü'ǖ ĩ ya wẽgü.
+/// 37 Rü ngeguma cuütanütchaǖgu rü daani'ĩ ya maẽ nhanagürü
+/// 38 - rü yeguma rü inayau ga maẽ ga Mapana.
+/// 39 - Rü yeguma ga noẽ i coou ta ngica itarü ĩ,
+/// 40 rü ngiǖ ta ngaǖ rü cu tücü,
+/// 41 pa tchauta'ã nhãtagürü:
+/// 42 - Natürü ga guma maẽ rü naega,
+/// 43 rü ngerata ni'ĩ ga naẽga,
+/// 44 natütü guma maẽ,
+/// 45 rü na taütchirema.
+///  46 Rü natürü yiema i daugu rü na i'ra.
+/// 47 Rü yeguma i nayau ga maẽ,
+/// 48 rü ngigürügü taũǖ tama ni'ĩ nua.
+/// 49 cuǖna ngẽẽǖ ngigürügü:
+/// 50 Rü marü yemawena ga coou rü tüna i ĩgü rü weriǖ ti'igüãrü.
+/// 51 Rü yeguma ga nüma ga Ngutapa rü marü na taegutchaǖ.
+/// 52 Rü yeguma marü nataegugu rü wowerugu ni'ĩ fetchigü,
+/// 53 rü yeguma ga ngima ga Mapana rü marü iya nguẽẽ ga namacüwawa ga naĩpünewa rü nüma ga Ngutapa rü ni'ĩ woweruetchigü.
+/// 54 Rü ni'ĩ wü'i paratchigü.
+/// 55 rü ni'ĩ fenagütchigü,
+/// 56 rü nhãnagürü:
+/// 57 - nhũmarütchi nhũnhã i Mapana?!
+/// 58 i maẽ ngi'ǖ wiarü ngaǖãcü rü ẽne ngi'ǖ ngo arü nga'uãcü nhãnagürü!
+/// 59 Tcherutcheru-u-u-u ... tcherurutcheruru-u-u-u ... - nhanagürü.
+/// 60 Rü yeguma ga tüma ga Mapana rü marü nüǖ ta inü'ǖ,
+/// 61 Rü wüiparagu ni'ĩ ga nüma rü yeguma marü na ngupetüatchigu.
+/// 62 rü nügü i'ya caiãtchigu rü yeguma naweama nenanhãga guma maẽ.
+/// 63 Rü pe ga naã pü'ügu rü norü naiãpüüguta.
+/// 64 Rü ngǖ ga ini'ĩ ni'ĩngu,
+/// 65 rü marü tama yema i natchi rü tüma ga norü na'ma ga Mapana,
+/// 66 rü tüma i ti'u rü yema yirüma ni'ĩ rü nüna iti'ãma ga ĩpatawa.
+/// 67 Rü yeguma ga nüma rü marü taguma i natchi rü nüma ga naãpüǖ rü marü manarütchaapüǖ,
+/// 68 rü yemaãcü tama ini'ĩu rü ini waatchigü,
+/// 69 Rü nhumata inangu ga ĩpatawa ga nayaanegü.
+""" 
 
 texto_ticuna = """TORÜ DUǕ'ǕGÜ
-
-Nori arü ügü ga ore ga nucüma'ǖ ga torü ga na nhunhaacü yi'i'ǖ.
-Norü ügü tchiga
-
-/01 Nüma ga Ngutapa ga naãne nama'ã ya ĩĩtchicü, rü nge'eǖ rü na tauma i ta'acü i nhũma na yema i na i aẽǖrü'ǖ rü yiĩ'ǖ ngema na yamareǖ ga Ngutapa.
-/02 I nori'ǖtchicü, ingeguma, rü norü tchiura ta ngemana yaǖ, rü ya Baia rü ta cünanangemaǖ rü norü tchiura rü ta nangemaǖ, erü nüma ga Baia rü Ngutapa tanü'ǖ ni'ĩ rü wü'iva na yae.
-/03 Nucüma ngeguma rü numa ga Ngutapa rü na tauǖma ga norü bue nu'ǖ tayema yerü tauguma nügüga nidau, ga namamaã rü na yaemare.
-/04 Ngeguma yema namamare i ya'ĩ, rü tauguma i buǖã, ga ngĩma ga Mapana, rü yemaca ngitchi naai ga nüma ga Ngutapa.
-/05 Inacü i ngeguma i ngema, i Taiwegüne i nori naãene i üǖwacü, rü ngema ni'ĩ i nangemagüǖ erü ngema i nori naü'ǖ i naãene nua natü i Tonetü.
-/06 Erü yeguma ãrü naãenecü rü wü'i i ngewaca i ya eaneǖrǖ'ǖ ni'ĩ yerü natauma ga nhãtü, rü yema nawa nayemagüǖ ricatama ni'ĩ.
-/07 Rü yeguma ngemamã ni'ĩ i nangemagü'ǖ rü marü nhurema ya taunecǖ na yemagü, rü yegumama ni'ĩ ga tüǖ nacuaiǖ ga namã ga Ngutapa, rü yeguma rü na tauma ga to, ga duǖ'ǖ nüĩcagümare ni'ĩ ga nayemagü'ǖ.
-/08 Natürü ga guma Baia rü nü'ǖ tayema ga bue, rü naaãcü nǖetcha nüma ga guma.
-/09 Ngutapa rü tauguma naãcü, rü mu'utchicüma ya tauenecü na ngeacü.
-/10 Rü yeguma fenewa tümamãã na u'ǖ ga namá ga Ngutapa yerü yeguma rü marü nayaanegü ga naĩnecü, rü yema i na feneeügu ni'ĩ ga tü'ǖ na cuai'ǖ ga nama, rü yeguma tü'ǖ ya naĩ'ü, rü nainewa tü'ǖ ni'ĩ nai rü ngiparawa rüta, tü'ǖ ni'ĩ naĩgü, rü ngĩtchacü'ǖwa rüta. 
-/11 Rü yemawena rü nüma ga Ngutapa rü na üpetü ga na ya feneǖ.
-/12 Yerü yegumarü marü auriǖtchima nguǖ tinge, rü mae rü tü'ǖ nawi rü ẽne, rü tü'ü na ngo ga tümaãrü ngaǖwa.
-/13 Rü Yeguma tümaetügu i na rü wagügü i coou rü ngeguma ngina tacagü rü nhãtagürü, pa noẽ pa coou tautchiname'ǖ ega tchoǖ icuwẽgǖgu nhãtagürü co-co-co-cou nhatagüǖ, pa noẽ tauchina meǖ ega tchoǖ icuiwẽgügu rü tchoǖ ni'ĩ ma, i ucaǖǖtchi i Ngutapa nhãtagürü.
-/14 Rü yeguma ga noẽ i coou ta ngica itarü ĩ rü ngiǖ ta ngaǖ rü cu tücü pa tchauta'ã nhãtagürü.
-/15 Rü yeguma ga noẽ ga coocou rü ĩrü duǖ.
-/16 Rü yeguma rü tü'ǖ ĩ ya wẽgü rü ngeguma cuütanütchaǖgu rü daani'ĩ ya maẽ nhanagürü, rü yeguma rü inayau ga maẽ ga Mapana.
-/17 Natürü ga guma maẽ rü naega rü ngerata ni'ĩ ga naẽga, natütü guma maẽ rü na taütchirema rü natürü yiema i daugu rü na i'ra.
-/18 Rü yeguma i nayau ga maẽ, rü ngigürügü taũǖ tama ni'ĩ nua cuǖna ngẽẽǖ ngigürügü.
-/19 Rü marü yemawena ga coou rü tüna i ĩgü rü weriǖ ti'igüãrü.
-/20 Rü yeguma ga nüma ga Ngutapa rü marü na taegutchaǖ, rü yeguma marü nataegugu rü wowerugu ni'ĩ fetchigü rü yeguma ga ngima ga Mapana rü marü iya nguẽẽ, ga namacüwawa ga naĩpünewa.
-/21 Rü nüma ga Ngutapa rü ni'ĩ woweruetchigü rü ni'ĩ wü'i paratchigü, rü ni'ĩ fenagütchigü, rü nhãnagürü, nhũmarütchi nhũnhã i Mapana i mae ngi'ǖ wiarü ngaǖãcü rü ẽne ngi'ǖ ngo arü nga'uãcü nhãnagürü tcherutcheru-u-u-u ... tcherurutcheruru-u-u-u ... nhanagürü.
-/22 Rü yeguma ga tüma ga Mapana rü marü nüǖ ta inü'ǖ rü wüiparagu ni'ĩ ga nüma, rü yeguma marü na ngupetüatchigu rü nügü i'ya caiãtchigu, rü yeguma naweama nenanhãga guma maẽ, rü pe ga naã pü'ügu rü norü naiãpüüguta.
-/23 Rü ngǖ ga ini'ĩ ni'ĩngu, rü marü tama yema i natchi rü tüma ga norü na'ma ga Mapana, rü tüma i ti'u rü yema yirüma ni'ĩ rü nüna iti'ãma ga ĩpatawa. Rü yeguma ga nüma rü marü taguma i natchi rü nüma ga naãpüǖ rü marü manarütchaapüǖ, rü yemaãcü tama ini'ĩu rü ini waatchigü, rü nhumata inangu ga ĩpatawa ga nayaanegü.
+/// 01 Nüma ga Ngutapa ga naãne nama'ã ya ĩĩtchicü,
+/// 02 rü nge'eǖ rü na tauma i ta'acü i nhũma na yema i na i aẽǖrü'ǖ.
+/// 03 Rü yiĩ'ǖ ngema na yamareǖ ga Ngutapa.
+/// 04 I nori'ǖtchicü,
+/// 05 ingeguma,
+/// 06 rü norü tchiura ta ngemana yaǖ,
+/// 07 rü ya Baia rü ta cünanangemaǖ rü norü tchiura rü ta nangemaǖ.
+/// 08 Erü nüma ga Baia rü Ngutapa tanü'ǖ ni'ĩ rü wü'iva na yae.
+/// 09 Nucüma ngeguma rü numa ga Ngutapa rü na tauǖma ga norü bue nu'ǖ tayema yerü tauguma nügüga nidau,
+/// 10 ga namamaã rü na yaemare.
+/// 11 Ngeguma yema namamare i ya'ĩ,
+/// 12 rü tauguma i buǖã.
+/// 13 Ga ngĩma ga Mapana,
+/// 14 rü yemaca ngitchi naai ga nüma ga Ngutapa.
+/// 15 Inacü i ngeguma i ngema.
+/// 16 I Taiwegüne i nori naãene i üǖwacü rü ngema ni'ĩ i nangemagüǖ erü ngema i nori naü'ǖ i naãene nua natü i Tonetü.
+/// 17 Erü yeguma ãrü naãenecü rü wü'i i ngewaca i ya eaneǖrǖ'ǖ ni'ĩ yerü natauma ga nhãtü rü yema nawa nayemagüǖ ricatama ni'ĩ.
+/// 18 Rü yeguma ngemamã ni'ĩ i nangemagü'ǖ,
+/// 19 rü marü nhurema ya taunecǖ na yemagü rü yegumama ni'ĩ ga tüǖ nacuaiǖ ga namã ga Ngutapa rü yeguma rü na tauma ga to,
+/// 20 ga duǖ'ǖ nüĩcagümare ni'ĩ ga nayemagü'ǖ.
+/// 21 Natürü ga guma Baia rü nü'ǖ tayema ga bue rü naaãcü nǖetcha nüma ga guma.
+/// 22 Ngutapa rü tauguma naãcü rü mu'utchicüma ya tauenecü na ngeacü.
+/// 23 Rü yeguma fenewa tümamãã na u'ǖ ga namá ga Ngutapa,
+/// 24 yerü yeguma rü marü nayaanegü ga naĩnecü rü yema i na feneeügu ni'ĩ ga tü'ǖ na cuai'ǖ ga nama,
+/// 25 rü yeguma tü'ǖ ya naĩ'ü rü nainewa.
+/// 26 tü'ǖ ni'ĩ nai rü ngiparawa rüta tü'ǖ ni'ĩ naĩgü rü ngĩtchacü'ǖwa rüta.
+/// 27 Rü yemawena rü nüma ga.
+/// 28 Ngutapa rü na üpetü ga na ya feneǖ.
+/// 29 Yerü yegumarü marü auriǖtchima nguǖ tinge rü maẽ rü tü'ǖ nawi rü ẽne rü tü'ü na ngo ga tümaãrü ngaǖwa.
+/// 30 Rü Yeguma tümaetügu i na rü wagügü i coou rü ngeguma ngina tacagü rü nhãtagürü:
+/// 31 - Pa noẽ,
+/// 32 pa coou tautchiname'ǖ ega tchoǖ icuwẽgǖgu nhãtagürü co-co-co-cou nhatagüǖ?
+/// 33 - tauchina meǖ ega tchoǖ icuiwẽgügu rü tchoǖ ni'ĩ ma i ucaǖǖtchi i Ngutapa nhãtagürü:
+/// 34 - Rü yeguma ga noẽ ga Co, co, co, cou rü ĩrü duǖ!
+/// 35 - pa noẽ,
+/// 36 Rü yeguma rü tü'ǖ ĩ ya wẽgü.
+/// 37 Rü ngeguma cuütanütchaǖgu rü daani'ĩ ya maẽ nhanagürü
+/// 38 - rü yeguma rü inayau ga maẽ ga Mapana.
+/// 39 - Rü yeguma ga noẽ i coou ta ngica itarü ĩ,
+/// 40 rü ngiǖ ta ngaǖ rü cu tücü,
+/// 41 pa tchauta'ã nhãtagürü:
+/// 42 - Natürü ga guma maẽ rü naega,
+/// 43 rü ngerata ni'ĩ ga naẽga,
+/// 44 natütü guma maẽ,
+/// 45 rü na taütchirema.
+/// 46 Rü natürü yiema i daugu rü na i'ra.
+/// 47 Rü yeguma i nayau ga maẽ,
+/// 48 rü ngigürügü taũǖ tama ni'ĩ nua.
+/// 49 cuǖna ngẽẽǖ ngigürügü:
+/// 50 Rü marü yemawena ga coou rü tüna i ĩgü rü weriǖ ti'igüãrü.
+/// 51 Rü yeguma ga nüma ga Ngutapa rü marü na taegutchaǖ.
+/// 52 Rü yeguma marü nataegugu rü wowerugu ni'ĩ fetchigü,
+/// 53 rü yeguma ga ngima ga Mapana rü marü iya nguẽẽ ga namacüwawa ga naĩpünewa rü nüma ga Ngutapa rü ni'ĩ woweruetchigü.
+/// 54 Rü ni'ĩ wü'i paratchigü.
+/// 55 rü ni'ĩ fenagütchigü,
+/// 56 rü nhãnagürü:
+/// 57 - nhũmarütchi nhũnhã i Mapana?!
+/// 58 i maẽ ngi'ǖ wiarü ngaǖãcü rü ẽne ngi'ǖ ngo arü nga'uãcü nhãnagürü!
+/// 59 Tcherutcheru-u-u-u ... tcherurutcheruru-u-u-u ... - nhanagürü.
+/// 60 Rü yeguma ga tüma ga Mapana rü marü nüǖ ta inü'ǖ,
+/// 61 Rü wüiparagu ni'ĩ ga nüma rü yeguma marü na ngupetüatchigu.
+/// 62 rü nügü i'ya caiãtchigu rü yeguma naweama nenanhãga guma maẽ.
+/// 63 Rü pe ga naã pü'ügu rü norü naiãpüüguta.
+/// 64 Rü ngǖ ga ini'ĩ ni'ĩngu,
+/// 65 rü marü tama yema i natchi rü tüma ga norü na'ma ga Mapana,
+/// 66 rü tüma i ti'u rü yema yirüma ni'ĩ rü nüna iti'ãma ga ĩpatawa.
+/// 67 Rü yeguma ga nüma rü marü taguma i natchi rü nüma ga naãpüǖ rü marü manarütchaapüǖ,
+/// 68 rü yemaãcü tama ini'ĩu rü ini waatchigü,
+/// 69 Rü nhumata inangu ga ĩpatawa ga nayaanegü.
                 """
 
 
