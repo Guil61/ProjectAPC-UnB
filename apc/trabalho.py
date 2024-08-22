b46957fec037c5c5e0f5d1a190f5d0cdfcb936b4
Letícia Souza <138714479+LetiSouza18@users.noreply.github.com>
2024-08-22 18:22:58 -0300

    Update trabalho.py
    Adicionamos mais verificações

--- apc/trabalho.py
+++ apc/trabalho.py
@@ -1,14 +1,20 @@
 from textos1 import texto_port, texto_ticuna, texto_instrucoes
 
 
-print(texto_instrucoes)
+print("Seja bem-vindo(a) ao nosso correlacionador de frases português/ticuna.\n")
 print("1 - Traduzir frase em português para ticuna")
 print("2 - Traduzir frase em ticuna para português")
+print("9 - Sair do programa")
 
-escolha = input("Digite a opção desejada: ")
+escolha = input("\nDigite a opção desejada: ")
 
-print("Siga as seguintes instruções: ")
+if escolha != '1' and escolha != '2' and escolha != '9':
+        print("Opção inválida, tente novamente.")
+        escolha = input("\nDigite a opção desejada: ")
+elif escolha == '9':
+    quit()
 
+print("Siga as seguintes instruções: \n", texto_instrucoes)
 frase = input("Insira a frase que deseja traduzir: ")
 
 while len(frase) <= 4 or (texto_port.find(frase) == -1 and texto_ticuna.find(frase) == -1):
@@ -16,8 +22,6 @@ while len(frase) <= 4 or (texto_port.find(frase) == -1 and texto_ticuna.find(fra
         print("Frase muito curta, por favor, tente novamente.")
     elif texto_port.find(frase) == -1:
         print("A frase não está no texto, por favor, tente novamente.")
-    elif escolha != 1 or escolha != 2 or escolha != 9:
-        print("Opção inválida, tente novamente.")
 
     frase = input("Insira a frase que deseja traduzir: ")
 
@@ -25,14 +29,20 @@ while len(frase) <= 4 or (texto_port.find(frase) == -1 and texto_ticuna.find(fra
 if escolha == '1':       
     num = texto_port[texto_port.find(frase) - 3] + texto_port[texto_port.find(frase) - 2]
 
-    num_ticuna = int(texto_ticuna.find(num))
+    pos_num_ticuna = int(texto_ticuna.find(num))
 
-    prox_num_ticuna = int(texto_ticuna.find(str(int(num) + 1)))
+    pos_prox_num_ticuna = int(texto_ticuna.find(str(int(num) + 1)))
 
-    tam_frase_ticuna = prox_num_ticuna - num_ticuna
+    tam_frase_ticuna = pos_prox_num_ticuna - pos_num_ticuna
 
-    frase_traduzida =  texto_ticuna[num_ticuna + 2 : num_ticuna + tam_frase_ticuna - 1] 
-
+    frase_traduzida =  texto_ticuna[pos_num_ticuna + 2 : pos_num_ticuna + tam_frase_ticuna - 1] 
+    
+    if tam_frase_ticuna > len(frase):
+        print("\nA frase está incompleta, mas corresponde à parte da seguinte frase em ticuna: \n")
+    
+    else:
+        print("\n A frase corresponde em ticuna é: \n")
+    
     print(frase_traduzida)
 
 
@@ -40,16 +50,21 @@ if escolha == '1':
 if escolha == '2':
     num = texto_ticuna[texto_ticuna.find(frase) - 3] + texto_ticuna[texto_ticuna.find(frase) - 2]
 
-    num_port = int(texto_port.find(num))
+    pos_num_port = int(texto_port.find(num))
 
-    prox_num_port = int(texto_port.find(str(int(num) + 1)))
+    pos_prox_num_port = int(texto_port.find(str(int(num) + 1)))
 
-    tam_frase_port = prox_num_port - num_port
+    tam_frase_port = pos_prox_num_port - pos_num_port
 
-    frase_traduzida =  texto_port[num_port + 2 : num_port + tam_frase_port - 1] 
-
+    frase_traduzida =  texto_port[pos_num_port + 2 : pos_num_port + tam_frase_port - 1]
+    
+    if tam_frase_port > len(frase):
+        print("\nA frase está incompleta, mas corresponde à parte da seguinte frase em português: \n")
+    
+    else:
+        print("\n A frase corresponde em português é: \n")
+    
     print(frase_traduzida)
-
 
 
 
